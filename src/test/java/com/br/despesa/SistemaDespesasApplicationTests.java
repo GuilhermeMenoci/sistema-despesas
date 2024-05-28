62a15fd4aeb10a45ac4e53e9d57fb0662f6a1a72
Guilherme <guimenoci@gmail.com>
2024-05-28 18:16:06 -0300

    feature: iniciando projeto

---
+++ src/test/java/com/br/despesa/SistemaDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.despesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
