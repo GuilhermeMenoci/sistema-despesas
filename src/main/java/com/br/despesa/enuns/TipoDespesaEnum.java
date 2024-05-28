e65ae7f84029d8495980b17c950a32cfd174b519
Guilherme <guimenoci@gmail.com>
2024-05-28 19:11:49 -0300

    fix: adicionando coluna descricao na entidade de movimentacao

--- src/main/java/com/br/despesa/enuns/TipoDespesaEnum.java
+++ src/main/java/com/br/despesa/enuns/TipoDespesaEnum.java
@@ -1,14 +1,20 @@
 package com.br.despesa.enuns;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public enum TipoDespesaEnum {
 
-	TP_EDUCACAO,
-	TP_INVESTIMENTO,
-	TP_LAZER,
-	TP_VIAGEM,
-	TP_OUTROS;
+	TP_EDUCACAO("Educação"),
+	TP_INVESTIMENTO("Investimento"),
+	TP_LAZER("Lazer"),
+	TP_VIAGEM("Viagem"),
+	TP_OUTROS("Outros");
+	
+	private String descricao;
 	
 }
