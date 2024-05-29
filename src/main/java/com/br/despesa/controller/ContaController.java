40a85f822aa840866af0576a22fadc242c8ac80e
Guilherme <guimenoci@gmail.com>
2024-05-29 16:57:58 -0300

    feature: gerar arquivo xls com todas movimentacoes

--- src/main/java/com/br/despesa/controller/ContaController.java
+++ src/main/java/com/br/despesa/controller/ContaController.java
@@ -1,10 +1,15 @@
 package com.br.despesa.controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.despesa.dto.request.ContaRequest;
@@ -28,6 +33,16 @@ public class ContaController {
 	 @GetMapping("/{id}")
 	 public ContaResponse buscarContaPorId(@PathVariable Long id) {
 		 return contaService.buscarContaPorId(id);
+	 }
+	 
+	 @GetMapping("/{id}/relatorios/despesas")
+	 @ResponseBody
+	 public ResponseEntity<Resource> gerarPdfEtiquetaUp(@PathVariable Long id) {
+		 HttpHeaders headersOut = new HttpHeaders();
+		 headersOut.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+		 headersOut.setContentDispositionFormData("attachment", "despesas.xls");
+
+		 return ResponseEntity.ok().headers(headersOut).body(contaService.gerarXlsDespesas(id));
 	 }
 	
 }
