2c1e9b6deec72f49e7f8e5db7457287ea5316baa
Guilherme <guimenoci@gmail.com>
2024-11-14 20:53:05 -0300

    fix: configuraçao swagger

--- src/main/java/com/br/despesa/config/SwaggerConfig.java
+++ src/main/java/com/br/despesa/config/SwaggerConfig.java
@@ -1,23 +1,23 @@
 package com.br.despesa.config;
 
-
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import lombok.Generated;
 
 @Generated
 @Configuration
 public class SwaggerConfig {
 
-	// http://localhost:8080/swagger-ui/index.html
 	@Bean
-	GroupedOpenApi swagger() {
-		return GroupedOpenApi.builder()
-				.group("com.br.despesa")
-				.packagesToScan("com.br.despesa")
-				.build();
+	OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Sistema de Despesas API")
+						.version("1.0")
+						.description("API para o sistema de gerenciamento de despesas"));
 	}
-	
+
 }
